94735eef76f6c7906b7cf3b64c1364d4b0ac0b5a
balazsNorbert <balazs.norbert@student.ms.sapientia.ro>
2025-02-12 18:55:43 +0200

    Add Muscle Group option to form and show muscle groups on weekly views too.

--- src/components/WeeklyView.js
+++ src/components/WeeklyView.js
@@ -18,6 +18,7 @@ const WeeklyView = ({ workouts, handleDeleteSet }) => {
   const [week, setWeek] = useState(getCurrentWeek(currentDay));
   const [showModal, setShowModal] = useState(null);
   const [deleteData, setDeleteData] = useState(null);
+  const [showMuscleGroups, setShowMuscleGroups] = useState(false);
 
   const handleNavigate = (direction) => {
     const today = new Date(currentDay);
@@ -46,6 +47,9 @@ const WeeklyView = ({ workouts, handleDeleteSet }) => {
 
   return (
     <div className="flex flex-col h-full gap-5 mb-20">
+      <button onClick={() => setShowMuscleGroups(!showMuscleGroups)} className="bg-gray-700 dark:bg-teal-600 hover:bg-teal-400 dark:hover:bg-teal-500 transition duration-300 px-4 py-2 w-fit mx-auto rounded-lg">
+        {showMuscleGroups ? "Show exercises" : "Show Muscle Group"}
+      </button>
       <div className="flex justify-between items-center w-full text-sm md:text-lg lg:text-xl">
         <button onClick={() => handleNavigate('prev')} className="bg-gray-700 dark:bg-teal-600 hover:bg-teal-400 dark:hover:bg-teal-500 transition duration-300 px-4 py-2 rounded-lg">
           Previous Week
@@ -60,31 +64,38 @@ const WeeklyView = ({ workouts, handleDeleteSet }) => {
           const dayNumber = new Date(date).getDate();
           const dayName = new Date(date).toLocaleDateString('en-US', { weekday: 'short' });
           return (
-            <div key={index} className={`aspect-square pt-1 px-1 md:px-3 pb-11 md:pb-14 lg:pb-16 min-h-20 lg:min-h-52 rounded-lg shadow-xl ${isToday ? 'bg-teal-500' : 'bg-white dark:bg-teal-700 text-black dark:text-white'}`}>
-              <h3 className="text-xs lg:text-base font-semibold text-center">{dayName}</h3>
-              <p className="text-center text-sm lg:text-lg mb-1">{dayNumber}</p>
+            <div key={date} className={`aspect-square pt-1 px-1 md:px-3 pb-7 lg:pb-10 min-h-20 lg:min-h-52 rounded-lg shadow-xl ${isToday ? 'bg-teal-500' : 'bg-white dark:bg-teal-700 text-black dark:text-white'}`}>
+              <h3 className="text-xs lg:text-base font-semibold text-center mb-2">{dayNumber} {dayName}</h3>
               <div className="h-full overflow-y-auto overflow-x-auto w-full">
                 {workouts.filter((workout) => new Date(workout.date).toISOString().slice(0, 10) === date).map((workout, workoutIndex) => (
+                  showMuscleGroups ? (
+                    <div key={`${date}-${workoutIndex}`} className="flex flex-col gap-1 md:gap-4">
+                      {[...new Set(workout.exercise.map((exercise) => exercise.muscleGroup))].map((muscle, muscleIndex) => (
+                        <span key={`${muscle}-${muscleIndex}`} className={`text-center ${isToday ? 'bg-teal-700 dark:bg-teal-700' : 'bg-teal-600 dark:bg-teal-500 text-white'}
+                        p-2 rounded-md shadow-md text-sm md:text-xl 2xl:text-2xl font-bold`}>{muscle}</span>
+                      ))}
+                    </div>
+                  ) : (
                   <ul className="flex gap-3" key={`${date}-${workoutIndex}`}>
                     {workout.exercise.map((exercise, exerciseIndex) => (
-                      <li key={exerciseIndex} className="flex gap-1 text-xs md:text-sm 2xl:text-lg">
+                      <li key={exercise._id} className={`flex gap-1 text-xs md:text-sm 2xl:text-lg pr-1 border-r-2 ${isToday ? 'border-white' : 'border-black dark:border-white'}`}>
                         <div className="flex flex-col min-w-max">
-                          <p className="font-semibold text-sm md:text-base 2xl:text-xl">
+                          <p className={`font-semibold text-xs md:text-base 2xl:text-xl border-b-2 ${isToday ? 'border-white' : 'border-black dark:border-white'}`}>
                             {`${exerciseIndex + 1}. `}
                             <Link to={`/exercise/${exercise.name}`}>{exercise.name}</Link>
                           </p>
                           <ul>
                             {exercise.sets.map((set, setIndex) => (
-                              <li key={setIndex} className="flex gap-1 items-center text-xs md:text-sm 2xl:text-lg">
+                              <li key={set._id} className="flex gap-1 items-center text-xxs md:text-sm 2xl:text-lg">
                                 <span className="font-semibold">{`${setIndex + 1}.`}</span>
                                 <p>{set.weight} {set.unit} - {set.reps} reps</p>
                                 <button type="button" onClick={() => handleOpenModal(date, workoutIndex, exerciseIndex, setIndex)} className="text-red-600 hover:text-red-700 transition duration-300">
-                                  <span className="material-icons text-lg md:text-xl xl:text-2xl 2xl:text-3xl">delete</span>
+                                  <span className="material-icons text-base md:text-xl xl:text-2xl 2xl:text-3xl">delete</span>
                                 </button>
                                 {showModal === setIndex && (
                                 <div className="fixed inset-0 flex justify-center items-center mx-8">
                                   <div className="flex flex-col gap-2 bg-teal-700 dark:bg-teal-600 p-5 rounded-lg shadow-lg">
-                                    <h2 className="font-semibold">Do you really want to delete this set?</h2>
+                                    <h2 className="font-semibold text-lg text-white">Do you really want to delete this set?</h2>
                                     <div className="bg-teal-800 hard:bg-teal-700 text-white p-2 rounded-md">
                                       <p className="text-base lg:text-xl font-bold">{set.weight} {set.unit} - {set.reps} reps</p>
                                     </div>
@@ -112,6 +123,7 @@ const WeeklyView = ({ workouts, handleDeleteSet }) => {
                       </li>
                     ))}
                   </ul>
+                  )
                 ))}
               </div>
             </div>
